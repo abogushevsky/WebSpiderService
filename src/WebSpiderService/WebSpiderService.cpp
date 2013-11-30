659fbbd95100e1035d9a1f194ec8271e499f5583
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2013-11-30 20:01:19 +0400

    Initial checkin

---
+++ src/WebSpiderService/WebSpiderService.cpp
@@ -0,0 +1,12 @@
+// WebSpiderService.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//SERVICE_TABLE_ENTRY ServiceTable[1];
+	return 0;
+}
+
