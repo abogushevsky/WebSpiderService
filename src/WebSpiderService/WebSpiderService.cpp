9cce361cad272dba2fc43f7daef5bf00e90c5712
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2013-11-30 20:07:25 +0400

    ServiceMain added

--- src/WebSpiderService/WebSpiderService.cpp
+++ src/WebSpiderService/WebSpiderService.cpp
@@ -1,12 +1,20 @@
 // WebSpiderService.cpp : Defines the entry point for the console application.
 //
 
+#include <Windows.h>
 #include "stdafx.h"
 
+#define SVCNAME TEXT("SvcName")
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	//SERVICE_TABLE_ENTRY ServiceTable[1];
+void ServiceMain(int argc, char** argv) {
+	return;
+}
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	SERVICE_TABLE_ENTRY ServiceTable[1];
+	ServiceTable[0].lpServiceName = SVCNAME;
+	ServiceTable[0].lpServiceProc = (LPSERVICE_MAIN_FUNCTION)ServiceMain;
+
 	return 0;
 }
 
