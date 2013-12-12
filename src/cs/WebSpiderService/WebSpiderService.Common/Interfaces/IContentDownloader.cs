d1d17c6a62a1a89c9985fbfa40561cadd00a15a6
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2013-12-12 22:19:23 +0400

    Common project & IContentDowloader added

---
+++ src/cs/WebSpiderService/WebSpiderService.Common/Interfaces/IContentDownloader.cs
@@ -0,0 +1,15 @@
+﻿namespace WebSpiderService.Common.Interfaces
+{
+    /// <summary>
+    /// Interface provides methods to download content of web pages
+    /// </summary>
+    public interface IContentDownloader
+    {
+        /// <summary>
+        /// Method dowloads page content
+        /// </summary>
+        /// <param name="url">Page url</param>
+        /// <returns>Page content</returns>
+        string DownloadUrl(string url);
+    }
+}
