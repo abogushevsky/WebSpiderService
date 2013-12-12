5ae852444b60fa17d3dac903e2c123886ad62979
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2013-12-12 22:24:06 +0400

    Initial version of ISpiderService added

---
+++ src/cs/WebSpiderService/WebSpiderService.Common/Interfaces/ISpiderService.cs
@@ -0,0 +1,13 @@
+﻿namespace WebSpiderService.Common.Interfaces
+{
+    /// <summary>
+    /// Interface describes a Web Spider
+    /// </summary>
+    public interface ISpiderService
+    {
+        /// <summary>
+        /// Method is intended to download all documents
+        /// </summary>
+        void DowloadDocuments();
+    }
+}
