c498f8c5d4b841722b218b3189b12d951fd0ecf8
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2013-12-15 20:57:34 +0400

    Comments added

--- src/cs/WebSpiderService/WebSpiderService.Impl/WebContentDownloader.cs
+++ src/cs/WebSpiderService/WebSpiderService.Impl/WebContentDownloader.cs
@@ -5,14 +5,28 @@ using WebSpiderService.Common.Interfaces;
 
 namespace WebSpiderService.Impl
 {
+    /// <summary>
+    /// Implementation of IContentDownloader that uses WebClient to download web pages
+    /// content from Web
+    /// </summary>
     public class WebContentDownloader : IContentDownloader
     {
+        /// <summary>
+        /// Method asynchronously dowloads page content
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         public async Task<string> DownloadUrlAsync(string url)
         {
             WebClient webClient = new WebClient();
             return await webClient.DownloadStringTaskAsync(new Uri(url));
         }
 
+        /// <summary>
+        /// Method dowloads page content
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         public string DownloadUrl(string url)
         {
             try
