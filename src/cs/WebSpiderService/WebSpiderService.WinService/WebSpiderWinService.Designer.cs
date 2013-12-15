1673e95b4f7ce0282a4b2832d4041ca8cf6b1cdc
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2013-12-15 20:25:38 +0400

    Win service project

---
+++ src/cs/WebSpiderService/WebSpiderService.WinService/WebSpiderWinService.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace WebSpiderService.WinService
+{
+    partial class WebSpiderWinService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webSpiderSvcController = new System.ServiceProcess.ServiceController();
+            // 
+            // webSpiderSvcController
+            // 
+            this.webSpiderSvcController.ServiceName = "WebSpiderWinService";
+            // 
+            // WebSpiderWinService
+            // 
+            this.ServiceName = "Service1";
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceController webSpiderSvcController;
+    }
+}
