fcd4c09953ecd08d0367fe8f4201c7ba78a3c6cb
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2013-12-25 19:16:25 +0400

    ISpiderControlService added

---
+++ src/cs/WebSpiderService/WebSpiderService.Wcf.Contract/Interfaces/ISpiderControlService.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+
+namespace WebSpiderService.Wcf.Contract.Interfaces
+{
+    /// <summary>
+    /// Interface describes a spider control service
+    /// </summary>
+    [ServiceContract]
+    public interface ISpiderControlService
+    {
+         
+    }
+}
