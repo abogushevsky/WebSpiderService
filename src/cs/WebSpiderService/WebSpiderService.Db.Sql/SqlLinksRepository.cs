f9e60357805546275560f0f9f65ede07df22b038
abogushevsky <anton.bogushevsky@gmail.com>
2013-12-30 17:33:32 +0400

    Lock removed

--- src/cs/WebSpiderService/WebSpiderService.Db.Sql/SqlLinksRepository.cs
+++ src/cs/WebSpiderService/WebSpiderService.Db.Sql/SqlLinksRepository.cs
@@ -11,35 +11,33 @@ namespace WebSpiderService.Db.Sql
             Contract.Requires(link != null);
             Contract.Requires(link.LinkContentType != null);
             Contract.Requires(!string.IsNullOrEmpty(link.LinkContentType.ContentType));
-            lock (this)
+
+            using (WebSpiderDBEntities context = new WebSpiderDBEntities())
             {
-                using (WebSpiderDBEntities context = new WebSpiderDBEntities())
+                ContentType contentType =
+                    context.ContentTypes.SingleOrDefault(
+                        ct => ct.LinkContentType == link.LinkContentType.ContentType);
+                if (contentType == null)
                 {
-                    ContentType contentType =
-                        context.ContentTypes.SingleOrDefault(
-                            ct => ct.LinkContentType == link.LinkContentType.ContentType);
-                    if (contentType == null)
+                    contentType = new ContentType()
                     {
-                        contentType = new ContentType()
-                        {
-                            LinkContentType = link.LinkContentType.ContentType
-                        };
-                        context.ContentTypes.Add(contentType);
-                    }
-
-                    Link newLink = new Link()
-                    {
-                        ContentType = contentType,
-                        ContentTypeId = contentType.Id,
-                        ParentId = link.ParentId,
-                        Url = link.Url
+                        LinkContentType = link.LinkContentType.ContentType
                     };
-
-                    context.Links.Add(newLink);
-                    context.SaveChanges();
-
-                    return newLink.Id;
+                    context.ContentTypes.Add(contentType);
                 }
+
+                Link newLink = new Link()
+                {
+                    ContentType = contentType,
+                    ContentTypeId = contentType.Id,
+                    ParentId = link.ParentId,
+                    Url = link.Url
+                };
+
+                context.Links.Add(newLink);
+                context.SaveChanges();
+
+                return newLink.Id;
             }
         }
     }
