ca8bf6a382418f68f4a4fdfa4a46e5650d8f4c0c
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2014-05-27 20:26:02 +0400

    Some domain model corrections

--- src/cs/WebSpiderService/WebSpiderService.Common/SocialNetworks/Entities/Person.cs
+++ src/cs/WebSpiderService/WebSpiderService.Common/SocialNetworks/Entities/Person.cs
@@ -7,6 +7,10 @@
     {
         public string Id { get; set; }
 
+        public string UserName { get; set; }
+
+        public string Name { get; set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
